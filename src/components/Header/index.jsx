1d2b9e230ced544d879d6fb94f9571b734c51846
Madera <MaderaHLN@hotmail.com>
2022-11-16 22:19:39 -0600

    fix: changed marquee tag and some styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,9 +4,9 @@ import './styles.css';
 export const Header = () => {
   return (
     <header className='header_container'>
-      <marquee className='header_title'>
+      <h2 className='header_title'>
         This Web Application is to Look For any Pokemon
-      </marquee>
+      </h2>
     </header>
   );
 };
